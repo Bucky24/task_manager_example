b059ee6565fc92228a439f3f5df739aa1f8b4215
Bucky24 <rwijtman@gmail.com>
2023-01-06 15:09:09 -0800

    create user api

--- src/server/controllers/api/user.js
+++ src/server/controllers/api/user.js
@@ -1,9 +1,31 @@
 const { Router } = require("express");
 
+const { User } = require('../../models');
+
 const userRouter = new Router();
 
 userRouter.post("/", async (req, res) => {
     const { username, password } = req.body;
+
+    const existingUser = await User.findOne({
+        where: {
+            username,
+        },
+    });
+
+    if (existingUser) {
+        res.status(409).end();
+        return;
+    }
+
+    const newUser = await User.create({
+        username,
+        password,
+    });
+
+    res.status(200).json({
+        id: newUser.id,
+    });
 });
 
 module.exports = userRouter;

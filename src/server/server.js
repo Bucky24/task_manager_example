7c849f4b0bc50715f789632b38d78bd9bcbd75bf
Bucky24 <rwijtman@gmail.com>
2023-01-06 15:02:17 -0800

    intiial login page and skeleton of server

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const sequelize = require("./config/connection");
+
+const allRouter = require("./controllers");
+require("./models");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "..", "client")));
+app.use(allRouter);
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    });
+});

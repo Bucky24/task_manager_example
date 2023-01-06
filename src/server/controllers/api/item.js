3f0bcfa816a8f85c45fdb462821aa8dd368b600c
Bucky24 <rwijtman@gmail.com>
2023-01-06 15:37:21 -0800

    item creation api and auth middleware

--- src/server/controllers/api/item.js
+++ src/server/controllers/api/item.js
@@ -1,5 +1,21 @@
 const { Router } = require("express");
 
+const auth = require("../../middleware/auth");
+const { Item } = require('../../models');
+
 const itemRouter = new Router();
+
+itemRouter.post('/', auth, async (req, res) => {
+    const { title } = req.body;
+
+    const newItem = await Item.create({
+        title,
+        UserId: req.user_id,
+    });
+
+    req.status(200).json({
+        id: newItem.id,
+    });
+});
 
 module.exports = itemRouter;
--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
-const sequelize = require("./config/connection");
+const cookieParser = require("cookie-parser");
 
+const sequelize = require("./config/connection");
 const allRouter = require("./controllers");
 require("./models");
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "..", "client")));
 app.use(express.json());
+app.use(cookieParser());
 app.use(allRouter);
 
 sequelize.sync().then(() => {
